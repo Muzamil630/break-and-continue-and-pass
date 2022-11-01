f344c7dd76bf89dbaec10c70d45489489a75c0b4
Muzamil630 <116160858+Muzamil630@users.noreply.github.com>
2022-11-01 14:59:11 +0530

    Add files via upload

---
+++ break and continue pass path2.py
@@ -0,0 +1,20 @@
+n=-1
+while(n<20):
+    n+=2
+    if n>8:
+        break
+    print(n)
+
+n=0
+while(n<20):
+    n+=1
+    if n==6:
+        break
+    print(n)
+
+n=0
+while(n<30):
+    n+=2
+    if(n==20):
+        break
+    print(n)
---
+++ break and countinue pass.py
@@ -0,0 +1,29 @@
+n=0
+while n<10:
+    n+=1
+    if n==7:
+        continue
+    print(n)
+
+
+
+
+n=0
+while n<10:
+    n+=1
+    if n>6:
+        continue
+    print(n)
+
+
+n=0
+while n<10:
+    n+=2
+    if n<=3:
+        continue
+    print(n)
+
+if (11>2):
+    pass
+else:
+    print('False')
